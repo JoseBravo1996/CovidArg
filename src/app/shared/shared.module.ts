4b6b69250f23925e53243456db472e8158cce528
JoseBravo1996 <bravo.jose.luis18@gmail.com>
2020-03-22 21:54:04 -0300

    add navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [],
+  declarations: [AppNavbarComponent],
   imports: [
-    CommonModule
+    RouterModule,
+    MatMenuModule,
+    MatToolbarModule,
+    FormsModule
+  ],
+  exports: [
+    AppNavbarComponent
   ]
 })
 export class SharedModule { }
